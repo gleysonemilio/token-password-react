e50146ca91bf9e09727153380400d2c2cefa9a38
gleysonemilio <gleyson.emilio@outlook.com>
2023-01-28 20:39:02 -0300

    feat: new componet React

---
+++ dist/FieldSteps.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="react" />
+import './style.css';
+interface Props {
+    numb: number;
+}
+export declare const FieldSteps: ({ numb }: Props) => JSX.Element;
+export {};
---
+++ src/FieldSteps.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './style.css'
+
+interface Props {
+  numb: number
+}
+
+export const FieldSteps = ({ numb }: Props) => {
+  return <div>TESTE COMPOMENT {numb} </div>
+}
